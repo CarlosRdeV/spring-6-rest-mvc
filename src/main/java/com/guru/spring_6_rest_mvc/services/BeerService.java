73979e6a4efedaefe480a3921831ecd7c0c8e593
Carlos Rivera <carlosrdev@gmail.com>
2024-07-07 18:23:47 -0600

    Refactor Service and create get all beers api
    Se refactorizo la implementación del servicio para crear manualmente instancias de beer y se creo el controlador para mostrar esos objetos en un api rest

--- src/main/java/com/guru/spring_6_rest_mvc/services/BeerService.java
+++ src/main/java/com/guru/spring_6_rest_mvc/services/BeerService.java
@@ -2,9 +2,12 @@ package com.guru.spring_6_rest_mvc.services;
 
 import com.guru.spring_6_rest_mvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
 }
 
