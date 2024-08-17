1fea3af07a77b28924f1f4b505d44f0107fda704
Carlos Rivera <carlosrdev@gmail.com>
2024-08-17 17:38:31 -0600

    create BeerOrderRepository and test

---
+++ src/test/java/com/guru/spring_6_rest_mvc/repositories/BeerOrderRepositoryTest.java
@@ -0,0 +1,49 @@
+
+package com.guru.spring_6_rest_mvc.repositories;
+
+import com.guru.spring_6_rest_mvc.entities.Beer;
+import com.guru.spring_6_rest_mvc.entities.BeerOrder;
+import com.guru.spring_6_rest_mvc.entities.Customer;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerOrderRepositoryTest {
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Customer testCustomer;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.findAll().getFirst();
+        testBeer = beerRepository.findAll().getFirst();
+
+
+    }
+
+    @Transactional
+    @Test
+    void testBeerOrders() {
+        BeerOrder beerOrder = BeerOrder.builder()
+                .customerRef("Test Order")
+                .customer(testCustomer)
+                .build();
+
+        BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
+
+        System.out.println(savedBeerOrder.getCustomerRef());
+
+    }
+}
